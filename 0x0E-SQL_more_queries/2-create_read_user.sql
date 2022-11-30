af5bbb4ccfa05f91f26241b203dabd763335ac3b
linco9ine <robeltilahun169@gmail.com>
2022-11-30 23:12:28 +0300

    more queries

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
 -- Create a datbase and and user with Select only privlage
-CREATE DATABASE IF NOT EXISTS hbtn_0d_1;
-CREATE USER user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
-GRANT SELECT ON hbtn_0d_1.* TO user_0d_1@localhost; 
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost; 
