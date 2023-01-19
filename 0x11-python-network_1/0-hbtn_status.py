2e7e0eee0c30e39a0a9d15e36aa5634a9d480aa7
linco9ine <robeltilahun169@gmail.com>
2023-01-19 17:16:47 +0300

    Python script that fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
-"""
-Fetches the URL: https://intranet.hbtn.io/status
+"""Fetches the URL: https://intranet.hbtn.io/status
 """
 
-if __name__ == '__main__':
-    import urllib.request
-    req = urllib.request.Request('https://intranet.hbtn.io/status')
-    with urllib.request.urlopen(req) as response:
-        html = response.read()
+from urllib.request import Request, urlopen
 
-    print("Body response:")
-    print("\t- type: {}".format(html.__class__))
-    print("\t- content: {}".format(html))
-    print("\t- utf8 content: {}".format(html.decode('ascii')))
+
+if __name__ == "__main__":
+    req = Request('https://intranet.hbtn.io/status')
+
+    with urlopen(req) as res:
+        content = res.read()
+        utf8_content = content.decode('utf-8')
+
+        print('Body response:')
+        print('\t- type: {_type}'.format(_type=type(content)))
+        print('\t- content: {_content}'.format(_content=content))
+        print('\t- utf8 content: {_utf8_c}'.format(_utf8_c=utf8_content))
